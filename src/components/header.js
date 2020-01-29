0309f82768def90b919a6680fddba5c46783588d
Marven Wilson Donque <marvenwilsons@gmail.com>
2020-01-29 10:48:00 -0500

    refactor header

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class header extends Component {
+  // methods
+  headerStye = () => {
+    return {
+      background: "#333",
+      color: "white",
+      paddingLeft: "25px",
+      paddingTop: "2px",
+      paddingBottom: "2px"
+    };
+  };
 
-    // methods
-    headerStye = () => {
-        return {
-            background: '#333',
-            color: 'white',
-            paddingLeft:'25px',
-            paddingTop:'2px',
-            paddingBottom:'2px'
-        }
-    }
-
-    // template
-    render() {
-        return (
-            <div style={this.headerStye()} >
-                <h1 style={{fontWeight:'100'}} > My Todo List </h1>
-            </div>
-        )
-    }
+  // template
+  render() {
+    return (
+      <div style={this.headerStye()}>
+        <h2
+          className="pacifico pad125"
+          style={{ fontWeight: "100", color: "white" }}
+        >
+          {" "}
+          My Todo List{" "}
+        </h2>
+      </div>
+    );
+  }
 }
 
-export default header
+export default header;
