e158a9f114208f34e80c0e78ae8821f626c5238f
Marven Wilson Donque <marvenwilsons@gmail.com>
2020-01-29 10:29:12 -0500

    use webFont api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 //bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import WebFont from "webfontloader";
 
 // my components
-import MyHeader from './components/header'
-import MyContent from './components/content'
+import MyHeader from "./components/header";
+import MyContent from "./components/content";
+
+WebFont.load({
+  google: {
+    families: ["Pacifico", "sans-serif"]
+  }
+});
 
 function App() {
   return (
-    <div style={{height:'100vh'}} className="App flex flexcol">
+    <div style={{ height: "100vh" }} className="App flex flexcol">
       <MyHeader />
-      <MyContent  />
+      <MyContent />
     </div>
   );
 }
