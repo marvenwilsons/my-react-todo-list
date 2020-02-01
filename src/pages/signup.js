810e873259b9fc42ea9a05a5ef2e689ee208d143
Marven Wilson Donque <marvenwilsons@gmail.com>
2020-01-31 21:26:56 -0500

    signup page ui

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Paper, Avatar } from "@material-ui/core";
+import { Button, Card, TextField, Divider } from "@material-ui/core";
 
 class componentName extends Component {
   // Name
@@ -9,7 +9,69 @@ class componentName extends Component {
   // intirest
   // not a robot
   render() {
-    return <div> textInComponent </div>;
+    return (
+      <div
+        style={{ background: "white" }}
+        className="fullheight-percent flex flexcenter"
+      >
+        <Card variant="outlined" style={{ width: "500px" }} className="pad125">
+          {/* lgogo */}
+          <div className="flex flexcenter padtop125 margintop125 marginbottom125">
+            <h4 className="pacifico">Coffe List</h4>
+          </div>
+          {/* sign up */}
+          <div className="flex flexcenter marginbottom125">
+            <h4>Sign Up</h4>
+          </div>
+          {/* form */}
+          <div className="pad125">
+            <div className="pad125">
+              <TextField
+                id="standard-basic"
+                className="fullwidth"
+                label="Full Name"
+              />
+            </div>
+            <div className="pad125">
+              <TextField
+                id="standard-basic"
+                className="fullwidth"
+                label="Email"
+              />
+            </div>
+            <div className="pad125">
+              <TextField
+                id="standard-basic"
+                className="fullwidth"
+                label="Username"
+              />
+            </div>
+            <div className="pad125">
+              <TextField
+                id="standard-basic"
+                className="fullwidth"
+                label="Password"
+                helperText="At least: 1 number, 1 uppercase letter & 1 special charater"
+                type="password"
+              />
+            </div>
+            <div className="pad125">
+              <TextField
+                id="standard-basic"
+                className="fullwidth"
+                label="Re type Password"
+                type="password"
+              />
+            </div>
+            <div className="pad125 flex flexend">
+              <Button variant="contained" color="primary">
+                Sign Up
+              </Button>
+            </div>
+          </div>
+        </Card>
+      </div>
+    );
   }
 }
 
